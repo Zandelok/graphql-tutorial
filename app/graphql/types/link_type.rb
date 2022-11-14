81d1233ccd0836eac206693ca98973edffc71c78
Zandelok <gleb.kirill98@gmail.com>
2022-11-14 08:15:40 +0300

    Commit-5: rubocop fixes

--- app/graphql/types/link_type.rb
+++ app/graphql/types/link_type.rb
@@ -2,8 +2,8 @@
 
 module Types
   class LinkType < Types::BaseObject
+    field :description, String, null: false
     field :id, ID, null: false
     field :url, String, null: false
-    field :description, String, null: false
   end
 end
