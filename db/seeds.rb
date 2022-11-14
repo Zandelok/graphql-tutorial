4588aef005a70473998e698dc054fafd5dfb4801
Zandelok <gleb.kirill98@gmail.com>
2022-11-14 09:43:47 +0300

    Commit-8: update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
-Link.create!(url: 'http://graphql.org/', description: 'The Best Query Language')
-Link.create!(url: 'http://dev.apollodata.com/', description: 'Awesome GraphQL Client')
+user = User.create!(name: 'Default user', email: 'some.mail@com', password: '123456')
+Link.create!(url: 'http://graphql.org/', description: 'The Best Query Language', user_id: user.id)
+Link.create!(url: 'http://dev.apollodata.com/', description: 'Awesome GraphQL Client', user_id: user.id)
