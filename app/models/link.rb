a71e3eda810e646bfaddca1b568c8d2553dd24cf
Zandelok <gleb.kirill98@gmail.com>
2022-11-14 13:05:49 +0300

    Commit-10: error handling for links and users

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,4 +3,7 @@
 class Link < ApplicationRecord
   belongs_to :user, optional: true
   has_many :votes, dependent: :destroy
+
+  validates :url, presence: true, length: { minimum: 5 }
+  validates :description, presence: true, length: { minimum: 5 }
 end
